315ca3f2c12e9ad24f5e51d456b83ef0b6912a61
REENA <javascriptcoding14@gmail.com>
2026-06-09 14:35:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let a=5;
+let b=10;
+let c=13;
+let d=56;
+ if((a%2)==0){                            //condition to check Number is odd or even
+    console.log(" The Number",a,"is Even.");
+}else{
+    console.log(" The Number",a,"is Odd.");
+}
+
+if((b%2)==0){                            //condition to check Number is odd or even
+    console.log(" The Number",b,"is Even.");
+}else{
+    console.log(" The Number",b,"is Odd.");
+}
+
+if((c%2)==0){                            //condition to check Number is odd or even
+    console.log(" The Number",c,"is Even.");
+}else{
+    console.log(" The Number",c,"is Odd.");
+}
+
+if((d%2)==0){                            //condition to check Number is odd or even
+    console.log(" The Number",d,"is Even.");
+}else{
+    console.log(" The Number",d,"is Odd.");
+}
